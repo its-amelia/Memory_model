e28c375293e5224bb2d7aed53dcc33523e58dcce
Amelia Mahdjoub <amelia.mahdjoub147@gmail.com>
2025-12-23 17:56:46 +0100

    ajout de la class

---
+++ src/Participant.java
@@ -0,0 +1,13 @@
+public class Participant {
+    private String categorie; // jeune vs vieux
+    private double valeurMemorisation;
+
+    public Participant(String categorie, double valeurMemorisation) {
+        this.categorie = categorie;
+        this.valeurMemorisation = valeurMemorisation;
+    }
+
+    
+
+
+}
