7fa78ca74b8a05aaee3fec69c831b75c04d90b12
Amelia Mahdjoub <amelia.mahdjoub147@gmail.com>
2025-12-23 18:24:14 +0100

    ajout oubli

--- src/Participant.java
+++ src/Participant.java
@@ -1,9 +1,29 @@
 public class Participant {
     private String categorie ; // jeune vs vieux
+    private double oubli; //valeur correpondant au pourcentage d'oubli d'une carte par tour
+
 
     public Participant(String categorie ) {
         this.categorie = categorie;
+        if (categorie.equals("jeune")) {
+            this.oubli = 0.1; //subjectif
+        } else if (categorie.equals("vieux")) {
+            this.oubli = 0.3; //subjectif
+        } else {
+            throw new IllegalArgumentException();
+        }
     
     }
+    public String getCategorie() {
+        return categorie;
+    }
+    public double getOubli() {
+        return oubli;
+    }
+
+
+
+
+
 
 }
