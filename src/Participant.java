ea4d0fa8264549305e468d1548adf39ce1cd0ef3
Amelia Mahdjoub <amelia.mahdjoub147@gmail.com>
2025-12-31 15:42:10 +0100

    up

--- src/Participant.java
+++ src/Participant.java
@@ -1,14 +1,17 @@
 public class Participant {
     private String categorie ; // jeune vs vieux
     private double oubli; //valeur correpondant au pourcentage d'oubli d'une carte par tour
+    private double renforcement; //si une carte est vue plusieurs fois elle est renfoée en mémoire , donc le taux d'oubli diminue 
 
 
     public Participant(String categorie ) {
         this.categorie = categorie;
         if (categorie.equals("jeune")) {
             this.oubli = 0.1; //subjectif
+            this.renforcement = 0.05; //subjectif
         } else if (categorie.equals("vieux")) {
             this.oubli = 0.3; //subjectif
+            this.renforcement = 0.02; //subjectif
         } else {
             throw new IllegalArgumentException();
         }
@@ -21,7 +24,10 @@ public class Participant {
         return oubli;
     }
 
-
+    public double getRenforcement() {
+        return renforcement;
+    }
+    
 
 
 
